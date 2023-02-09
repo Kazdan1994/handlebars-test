c43cfbf39c1ca24760d2b406948469676ea9c0f9
arthur.jacquemin <arthurjacquemin2@gmail.com>
2023-02-09 13:04:56 +0100

    feat: replace variables in body

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,11 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import { compile } from "handlebars";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// compile the template
+let body = document.querySelector("body");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+if (body) {
+  const template = compile(body.innerHTML);
+
+  // execute the compiled template and print the output to the console
+  body.innerHTML = template({ firstname: "Arthur", lastname: "Jacquemin" });
+}
